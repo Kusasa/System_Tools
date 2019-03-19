833b6983b6e90a44ca5a4e20b4738eb520a61566
Kusasa <skusasalethu@gmail.com>
2019-03-19 16:56:19 +0200

    back-up-service

---
+++ BackUpService.py
@@ -0,0 +1,22 @@
+"""------------------------------------------------------------------------------
+Script Name:      Back Up Service
+Version:          1.1
+Description:      This service uses a configured time to trigger the copying 
+                  of a file or a folder from one directory into another and then 
+                  updates a log file on the event.  
+Created By:       Kusasalethu Sithole
+Date:             2019-03-19
+Last Revision:    2019-03-19
+----------------------------------------------------------------------------------"""
+# Declare event() function
+    #copy folder from directory a to directory b
+    #capute event details in log file
+    # return timeCompare() function
+# Declare timeCompare()
+    # current_time = capture system time
+    # time_of_interest
+    # while current_time != time_of_interest:
+        #continue
+    # else:    
+        #return (event())
+
