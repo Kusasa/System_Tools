e4582dfb3440f86c379e01e63862ce4d1a4b6bc1
Kusasalethu Sithole <skusasalethu@gmail.com>
2019-10-09 19:38:04 +0200

    init

--- loop_xml_all_folders.bat
+++ loop_xml_all_folders.bat
@@ -1,3 +1,6 @@
 ECHO OFF
-ECHO cd "C:\Program Files (x86)\GeoServer 2.15.1\data_dir\workspaces\ramm-live-data"
-ECHO FOR /D /R %%G in (*) DO (cd %%G & python S:\KUSASA\GIS\gs_xml_rename\tools\update_workspace_id_in_xml.py)
+ECHO cd "C:\Program Files\GeoServer1_Tomcat\webapps\geoserver\data\workspaces\ramm-live-data"
+ECHO cd
+ECHO for /D /R %%G in (*) DO ( cd %%G && python "S:\KUSASA\RAMM_Enterprise_GIS\scripts\rename_xmls\update_password_in_xml.py" )
+ECHO ---------------------Process Completed-------------------
+PAUSE
